16758056bdaf55f21547b7f5f53844b90cd95e24
Amir Zarrinkafsh <nightah@me.com>
2022-07-22 15:49:20 +1000

    refactor: change prompt library to allow multi-selects on custom

--- go.mod
+++ go.mod
@@ -3,15 +3,20 @@ module lancache-diagnostics
 go 1.18
 
 require (
-	github.com/manifoldco/promptui v0.9.0
+	github.com/AlecAivazis/survey/v2 v2.3.5
 	github.com/miekg/dns v1.1.50
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c
+	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150
 )
 
 require (
-	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/tools v0.1.6-0.20210726203631-07bc1bf47fb2 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -5,17 +5,17 @@ import (
 	"os"
 	"strings"
 
-	"github.com/manifoldco/promptui"
+	"github.com/AlecAivazis/survey/v2"
 )
 
 func main() {
-	prompt := promptui.Select{
-		Label: "Select Mode",
-		Items: []string{"Diagnostics - Simple", "Diagnostics - Full", "Diagnostics - Custom", "Exit"},
+	result := ""
+	prompt := &survey.Select{
+		Message: "Select Mode:",
+		Options: []string{"Diagnostics - Simple", "Diagnostics - Full", "Diagnostics - Custom", "Exit"},
 	}
 
-	_, result, err := prompt.Run()
-
+	err := survey.AskOne(prompt, &result)
 	if err != nil {
 		fmt.Print(fmt.Errorf("error: prompt failed %w", err))
 		return
@@ -71,39 +71,40 @@ func full(servers []string) {
 func custom(servers []string) {
 selection:
 	file := ""
-	prompt := promptui.Select{
-		Label: "Select CDN",
-		Items: []string{"Back", "ArenaNet", "Blizzard", "Battle State Games", "City of Heroes", "Daybreak Games",
+	result := []string{}
+	prompt := &survey.MultiSelect{
+		Message: "Select CDN(s):",
+		Options: []string{"Back", "ArenaNet", "Blizzard", "Battle State Games", "City of Heroes", "Daybreak Games",
 			"Epic Games", "Frontier", "Neverwinter", "Nexus Mods", "Nintendo", "Origin", "Path of Exile",
 			"RenegadeX", "Riot Games", "Rockstar Games", "Sony", "SQUARE ENIX", "Steam", "The Elder Scrolls Online",
 			"UPlay", "Warframe", "WARGAMING", "Windows Updates", "Xbox Live", "Exit"},
+		PageSize: 20,
 	}
 
-	_, result, err := prompt.Run()
-
+	err := survey.AskOne(prompt, &result)
 	if err != nil {
 		fmt.Print(fmt.Errorf("error: prompt failed %w", err))
 		return
 	}
 
-	switch result {
-	case "Back":
+	switch {
+	case isStringInSlice("Back", result):
 		main()
-	case "Exit":
+	case isStringInSlice("Exit", result):
 		os.Exit(0)
 	default:
-		for _, cdn := range CDNs {
-			if cdn.Name == result {
-				file = cdn.File
-			} else {
-				continue
+		for _, cdn := range result {
+			for _, cdns := range CDNs {
+				if cdn == cdns.Name {
+					file = cdns.File
+					hostnames := parseCDN(cdn, file)
+					lookupHostnames("", hostnames, 1, servers)
+				} else {
+					continue
+				}
 			}
-			fmt.Printf("No matching CDN file found: %s\n", file)
 		}
 	}
-
-	hostnames := parseCDN(result, file)
-	lookupHostnames("", hostnames, 1, servers)
 
 	goto selection
 }
